87bb1044672039cfdecccff1cf6955f68698a9d9
susmithapatchigolla <43164408+susmithapatchigolla@users.noreply.github.com>
2021-11-10 22:05:08 +0530

    Susmitha

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/me/about">About final</Link>
+            <Link to="/me/about">About Susmitha</Link>
           </li>
           <li>
             <Link to="/dashboard">Dashboard Patchigolla</Link>
